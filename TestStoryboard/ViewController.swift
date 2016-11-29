b5366e7dc53f70d55d106f74b5c071e4f9305a89
rendy_hananta <rendy_hananta@yahoo.co.id>
2016-11-29 14:28:03 +0700

    initial commit

---
+++ TestStoryboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestStoryboard
+//
+//  Created by Rendy on 29/11/16.
+//  Copyright © 2016 RendycrazyProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        debugPrint("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
